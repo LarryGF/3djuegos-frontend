e58209c1577290e8dc6f984542d75ed458440d15
LarryGF <larrywtf609@gmail.com>
2019-05-23 02:08:18 -0400

    Made an infinite scroll and loading the games

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,67 @@
 
 <template>
-
-<v-layout >
-  {{games}}
-  <PathCard/>
-  
+  <v-layout row wrap v-if="games">
+    <!-- {{games[0]}} -->
+    <v-flex xs4 v-for="game in showGames" :key="game.name">
+      <v-layout column>
+        {{game.name}}
+        <v-img
+          :src="game.image"
+          contain
+          :max-height="0.3*height"
+          lazy-src="images/dissidia-012-prologus.jpg"
+        ></v-img>
+      </v-layout>
+    </v-flex>
   </v-layout>
+  <div v-else>loren</div>
 </template>
 
 <script>
-import PathCard from '../components/PathCard';
+import PathCard from "../components/PathCard";
 export default {
-  data () {
+  data() {
     return {
       height: 0,
       games: null,
-    }
+      showGames: [],
+      count: 0
+    };
   },
-  
-  watch: {
-  },
+
+  watch: {},
   components: {
     PathCard
   },
-  mounted () {
+  mounted() {
     // this.fetch()
-    this.height = window.innerHeight * 0.7
-    this.getInitialData()
+    this.height = window.innerHeight;
+    this.getInitialData();
+    this.scroll()
   },
   methods: {
-    getInitialData: async function(){
-    this.games = (await this.$axios.get('/db/db.json')).data
+    getInitialData: async function() {
+      this.games = (await this.$axios.get("/db/db.json")).data;
+      for (this.count; this.count < 21; this.count++) {
+        this.showGames.push(this.games[this.count]);
+      }
 
-    // console.log(this.$router)
+      // console.log(this.$router)
+    },
+    scroll: function() {
+      window.onscroll = () => {
+        let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight ;
 
-
+        if (bottomOfWindow){
+          for (var i=0;i<9;i++){
+            this.count++
+            this.showGames.push(this.games[this.count])
+          }
+        }
+      }
     }
   }
-}
+};
 </script>
 
 
