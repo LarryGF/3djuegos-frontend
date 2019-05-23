9bd1285b9915e9d6f559be678a193ecb2390c1cc
LarryGF <larrywtf609@gmail.com>
2019-05-23 17:37:18 -0400

    Added icons for each platform

--- components/Cover.vue
+++ components/Cover.vue
@@ -16,12 +16,21 @@
             class="d-flex transition-fast-in-fast-out  v-card--reveal  white--text"
             style="height: 100%; background-color:rgba(0,0,0,0.7)"
           >
-            <v-layout column  justify-center>
+            <v-layout column  justify-center text-xs-center>
             <v-layout row shrink justify-center>
-              <v-icon>
-                mdi-xbox
+              <v-flex xs4>
+
+              ({{game.gamePlatform}})
+              </v-flex>
+              <v-flex xs2>
+              <v-icon v-html="icon(game)">
               </v-icon>
+              </v-flex>
+              <v-flex xs8>
+
               {{game.name}}
+              </v-flex>
+
             </v-layout>
             </v-layout>
           </div>
@@ -48,6 +57,77 @@ export default {
     game: Object,
     height: Number,
     width: Number
+  },
+  methods:{
+    icon:function(item){
+      switch (item.gamePlatform){
+        case 'XBOX':
+        return 'mdi-xbox';
+        case 'PC':
+        return 'mdi-windows';
+        case 'PSP':
+        return 'mdi-gamepad-variant';
+        case 'Android':
+        return 'mdi-android';
+        case 'Nintendo Switch':
+        return 'mdi-nintendo-switch';
+        case 'DS':
+        return 'mdi-gamepad';
+        case 'iOS':
+        return 'mdi-apple-ios';
+        case 'PS1':
+        return 'mdi-playstation';
+        case 'PS2':
+        return 'mdi-playstation';
+        case 'PS3':
+        return 'mdi-playstation';
+        case 'PS4':
+        return 'mdi-playstation';
+        case 'PS5':
+        return 'mdi-playstation';
+        case '3DS':
+        return 'mdi-gamepad';
+        case 'Xbox 360':
+        return 'mdi-xbox';
+        case 'Wii':
+        return 'mdi-wii';
+        case 'NES':
+        return 'mdi-gamepad-circle';
+        case 'Vita':
+        return 'mdi-gamepad-variant';
+        case 'Web':
+        return 'mdi-web';
+        case 'Xbox One':
+        return 'mdi-xbox';
+        case 'Wii U':
+        return 'mdi-wiiu';
+        case 'GB':
+        return 'mdi-gamepad';
+        case 'N64':
+        return 'mdi-gamepad-round';
+        case 'GBA':
+        return 'mdi-gamepad';
+        case 'XBOX':
+        return 'mdi-xbox';
+        case 'Mac':
+        return 'mdi-apple-keyboard-command';
+        case 'GBC':
+        return 'mdi-gamepad';
+        case 'SNES':
+        return 'mdi-gamepad';
+        case 'GC':
+        return 'mdi-gamepad';
+        case 'Linux':
+        return 'mdi-linux';
+        case 'Móvil':
+        return 'mdi-cellphone-android';
+        case 'Xbox Scarlett':
+        return 'mdi-xbox';
+        
+        return 'mdi-gamepad'
+        
+      }
+    }
   }
 };
 </script>
